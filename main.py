b89101c4b9ed8bf43657545a86bf5047b5363ce0
Kevin Frans <kevin@bobthechicken.com>
2017-03-04 19:32:25 -0800

    fixes

--- main.py
+++ main.py
@@ -8,15 +8,7 @@ from random import randint
 
 from utils import *
 
-def imageblur(cimg, sampling=False):
-    if sampling:
-        cimg = cimg * 0.3 + np.ones_like(cimg) * 0.7 * 255
-    else:
-        for i in xrange(30):
-            randx = randint(0,205)
-            randy = randint(0,205)
-            cimg[randx:randx+50, randy:randy+50] = 255
-    return cv2.blur(cimg,(100,100))
+
 
 class Color():
     def __init__(self, imgsize=256, batchsize=4):
@@ -43,16 +35,12 @@ class Color():
         self.real_images = tf.placeholder(tf.float32, [self.batch_size, self.image_size, self.image_size, self.output_colors])
 
         combined_preimage = tf.concat(3, [self.line_images, self.color_images])
-        # combined_preimage = tf.concat([self.line_images, self.color_images], 3)
         # combined_preimage = self.line_images
 
         self.generated_images = self.generator(combined_preimage)
 
         self.real_AB = tf.concat(3, [combined_preimage, self.real_images])
         self.fake_AB = tf.concat(3, [combined_preimage, self.generated_images])
-
-        # self.real_AB = tf.concat([combined_preimage, self.real_images], 3)
-        # self.fake_AB = tf.concat([combined_preimage, self.generated_images], 3)
 
         self.disc_true, disc_true_logits = self.discriminator(self.real_AB, reuse=False)
         self.disc_fake, disc_fake_logits = self.discriminator(self.fake_AB, reuse=True)
